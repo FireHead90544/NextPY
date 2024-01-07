14e74fd438550f9bc68eecfecbd026b8ebba5b7f
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-07 16:21:30 +0530

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 		<div className="flex justify-center h-screen items-center">
-      <h1 className='font-thin text-3xl'><span className="">NextPY</span> - Initial Commit</h1>
+      <h1 className='font-thin text-3xl'><span className="font-semibold">NextPY</span> - Initial Commit</h1>
     </div>
 	);
 }

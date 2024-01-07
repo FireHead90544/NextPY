4d1c2beb8318cf75e255caeed319473d247c2719
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-07 19:33:54 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
 		<div className="flex justify-center h-screen items-center">
-      <h1 className='font-thin text-3xl'><span className="font-semibold">NextPY</span> - Initial Commit</h1>
-    </div>
+			<h1 className="font-thin text-3xl">
+				<span className="font-semibold">NextPY</span> - <Link href="/api/">API Routes</Link>
+			</h1>
+		</div>
 	);
 }

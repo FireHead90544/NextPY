d585e0cb2693fddf96047743a3714ee6fa93ea1d
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-09 22:48:54 +0530

    pack up later

--- app/api/get-download-links/route.ts
+++ app/api/get-download-links/route.ts
@@ -2,5 +2,7 @@ import { NextRequest } from "next/server";
 import { load } from "cheerio";
 
 export async function GET(request: NextRequest){
+    const id = request.nextUrl.searchParams.get("id");
+    // Will finish this endpoint later
     return Response.json({endpoint: "working"});
 }

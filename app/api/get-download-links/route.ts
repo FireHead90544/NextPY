cfc8e36263e4d0762ac937ad1ee701bc7946838c
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-10 23:58:33 +0530

    fixed edge cases

--- app/api/get-download-links/route.ts
+++ app/api/get-download-links/route.ts
@@ -15,17 +15,17 @@ export async function GET(request: NextRequest){
     const id = request.nextUrl.searchParams.get("id");
     let _ep = request.nextUrl.searchParams.get("ep");
     const ep: number | null = _ep !== null ? parseFloat(_ep) : null;
-    if (!id || !ep){ return Response.json({ id, ep, qualities: {} }) };
+    if (!id || ep===null){ return Response.json({ id, ep, qualities: {} }) };
     
     const resp = await fetch(`${BASE}/api/get-anime-episodes?id=${id}`);
     const eps: EpisodesResponse = await resp.json();
-    const episode_page: string | null = (eps.prefix && ep in eps.episodes) ? `${eps.prefix}${ep}` : null;
+    const episode_page: string | null = (eps.prefix && eps.episodes.includes(ep)) ? `${eps.prefix}${ep}` : null;
     if (!episode_page){ return Response.json({ id, ep, qualities: {} }) };
 
     const baseUrl = process.env.ANITAKU_BASE_URL;
     const cookies = { auth: process.env.ANITAKU_AUTH, gogoanime: process.env.ANITAKU_GOGOANIME };
 
-    const linkresp = await fetch(`${baseUrl}/${episode_page}`, { headers: { cookie: Object.entries(cookies).map(([k, v]) => `${k}=${v}`).join("; ") } });
+    const linkresp = await fetch(`${baseUrl}/${episode_page.replace('.', '-')}`, { headers: { cookie: Object.entries(cookies).map(([k, v]) => `${k}=${v}`).join("; ") } });
     const data = await linkresp.text();
     const $ = load(data);
     const qualities: { [key: string]: Promise<string | null> | string | null} = {}
@@ -43,10 +43,10 @@ export async function GET(request: NextRequest){
     Object.keys(qualities).forEach(async (key) => {
         if (qualities[key] instanceof Promise) {
         const result: any = await qualities[key];
-        qualities[key] = result instanceof Promise ? result : result;
+        qualities[key] = result instanceof Promise ? result : result.replace(result.split("&title=")[1], `&title=EP.${ep}.${key}_${id.replace('-', '_')}.mp4`);
         }
     });
     await Promise.allSettled(Object.values(qualities));
 
-    return Response.json({id, ep, qualities});
+    return Response.json({ id, ep, qualities });
 }

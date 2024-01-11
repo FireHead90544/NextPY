56374280511364e28b5e6d0a9e895e1a5946d8de
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-11 21:01:45 +0530

    get-cookies not finished yet

--- app/api/get-cookies/route.ts
+++ app/api/get-cookies/route.ts
@@ -1,5 +1,34 @@
 import { NextRequest } from "next/server";
+import { load } from "cheerio";
 
 export async function GET(request: NextRequest) {
-    return Response.json({ "status": "endpoint created" })
+    const email = request.nextUrl.searchParams.get("email");
+    const password = request.nextUrl.searchParams.get("password");
+    if (!email || !password) { return Response.json({ error: "email & password parameters missing!" }) };
+
+    const baseUrl = process.env.ANITAKU_BASE_URL;
+    const csrf = await (await fetch(`${baseUrl}/login.html`)).text();
+    const $ = load(csrf);
+    const _csrf = $("meta[name='csrf-token']").first().attr("content")!;
+
+    const login = await fetch(`${baseUrl}/login.html`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({ email, password, _csrf }).toString(),
+        credentials: "include",
+    })
+
+    // FIXME: Not finished yet. This logic doesn't work.
+    // Needs a session to fetch csrf_token first.
+    // Use the same session to login
+    // Extract the cookies from the session itself (not request)
+
+    const cookies = login.headers.get("set-cookie")!;
+    const gogoanime = cookies.match(/gogoanime=(.*?);/)?.[1];
+    const auth = cookies.match(/auth=(.*?);/)?.[1];
+    if (!gogoanime || !auth) { return Response.json({ error: "invalid credentials!" }) };
+
+    return Response.json({ gogoanime, auth });
 }

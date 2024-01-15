87c0a8e004b96000faa0e3ba8a001f06abc7cd4b
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-16 02:45:18 +0530

    Added: shadcn-ui, react-icons & updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
 		<div className="flex justify-center h-screen items-center">
-			<h1 className="font-thin text-3xl">
-				<span className="font-semibold">NextPY</span> - <Link href="/api/">API Routes</Link>
-			</h1>
+				<h1 className="font-thin text-3xl">
+					<span className="font-semibold">NextPY</span> -{" "}
+					<Link href="/">Stream & Download Animes</Link>
+				</h1>
+				<ThemeToggle />
 		</div>
 	);
 }

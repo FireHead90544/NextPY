8d176a5f45f2c67cde8ee1b1c246e4003e06459b
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-17 22:54:27 +0530

    Finish: Footer Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 export function Footer() {
   return (
-    <div>Footer</div>
+    <div className='pt-5 mb-12'>
+        <hr className='border-t border-stone-200 dark:border-stone-800'/>
+        <div className='flex justify-between mt-2'>
+            <div className='space-x-2'>
+                <span className='animate-wave inline-block origin-[70%_70%]'>👋</span>
+                <span>{new Date().getFullYear()}</span>
+                <span className='text-stone-300 dark:text-stone-700'>—</span>
+                <span className='font-thin'>こんにちは</span>
+            </div>
+            <Link className= 'flex space-x-2' href="https://github.com/FireHead90544/NextPY" target='_blank'>
+                <FaGithub className='my-auto' />
+                <span className='font-semibold underline underline-offset-4 decoration-stone-200 hover:decoration-stone-300 dark:decoration-stone-800 dark:hover:decoration-stone-700'>NextPY</span>
+            </Link>
+        </div>
+    </div>
   )
 }

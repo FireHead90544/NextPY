75c892fe8a7a4d14998981806dbeaf17a6d00505
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-17 22:03:03 +0530

    Updated header ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { ThemeToggle } from '@/components/ThemeToggle'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <div className="flex justify-between my-12">
+      <Link href="/">
         <h1 className="font-semibold text-3xl">
             <span className="font-semibold">NextPY</span>
         </h1>
-        <ThemeToggle />
+        <h1 className="font-thin">Stream & Download Animes</h1>
+      </Link>
+      <ThemeToggle />
     </div>
   )
 }

75b5649cb566de4d65402da70226e5ae2d3f629b
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-21 18:58:04 +0530

    Test

--- components/AnimeSearchList.tsx
+++ components/AnimeSearchList.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import { AnimeCard } from '@/components/AnimeCard'
 
 export function AnimeSearchList() {
   return (
-    <div className='pt-2 flex h-full rounded-md border space-x-2'>
-      { Array.from({ length: 10 }).map((_, i) =>
-        <div className='border rounded-md'>{ i+1 }</div>
-      )}
-    </div>
-  )
+		<div className="pt-2 flex h-full rounded-md border space-x-2">
+      {/* Test Component */}
+			<AnimeCard
+				released={null}
+				name="Ore dake Level Up na Ken"
+				image="https://gogocdn.net/cover/jujutsu-kaisen-tv-2nd-season-1688154932.png"
+				id="ore-dake-level-up-na-ken"
+			/>
+		</div>
+	);
 }

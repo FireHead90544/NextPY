ae5e33c2fd177eae5f14c58c2e62e488fb23851a
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-01-21 18:55:38 +0530

    Added gogo's cdn for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "gogocdn.net",
+                port: "",
+                pathname: "/cover/*",
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig

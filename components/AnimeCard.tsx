00cbc86c43be8215c577fa014cab0f157c317c9b
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-04-15 23:06:00 +0530

    bruh

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -17,11 +17,19 @@ interface AnimeCardProps {
 }
 
 export function AnimeCard({ name, id, image, released }: AnimeCardProps) {
-  // TODO: Mobile Devices: Convert Flex-Col to Flex-Row Based Layout (OK)
-  return (
-    <Card className='w-72 rounded-md p-4 space-y-3'>
-        <Image className="mx-auto rounded-md w-3/4" src={image} width={460} height={650} alt={ name } />
-        <CardTitle className='mx-auto text-center text-sm font-[350] leading-tight w-3/4'>{ name }</CardTitle>
-    </Card>
-  )
+	// TODO: Mobile Devices: Convert Flex-Col to Flex-Row Based Layout (OK ig)
+	return (
+		<Card className="w-72 rounded-md p-4 space-y-3">
+			<Image
+				className="mx-auto rounded-md w-3/4"
+				src={image}
+				width={460}
+				height={650}
+				alt={name}
+			/>
+			<CardTitle className="mx-auto text-center text-sm font-[350] leading-tight w-3/4">
+				{name}
+			</CardTitle>
+		</Card>
+	);
 }

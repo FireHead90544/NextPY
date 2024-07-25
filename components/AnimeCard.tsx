fbc1e92a80dd67b691b673ee5a7b3de4a57bfead
Rudransh Joshi <rudranshjoshi1806@gmail.com>
2024-07-25 21:59:01 +0530

    Update AnimeCard.tsx

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -17,7 +17,7 @@ interface AnimeCardProps {
 }
 
 export function AnimeCard({ name, id, image, released }: AnimeCardProps) {
-	// TODO: Mobile Devices: Convert Flex-Col to Flex-Row Based Layout (idek how-first sed finish when)
+	// TODO: Mobile Devices: Convert Flex-Col to Flex-Row Based Layout (idek how-first sed finish when when)
 	return (
 		<Card className="w-72 rounded-md p-4 space-y-3">
 			<Image
